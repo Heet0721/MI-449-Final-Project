27ca1131963b9d2c3950d8f33e54a4d98f568d85
Heet Barot <Barothee@msu.edu>
2023-04-28 16:18:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import MealList from "./MealList";
+import { createClient } from '@supabase/supabase-js';
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const supabaseUrl = 'https://mleazactnjtqqikybupq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sZWF6YWN0bmp0cXFpa3lidXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwODM3MjUsImV4cCI6MTk5NzY1OTcyNX0.U8XjoZGD3qWP3NeI-DqOlkA2ss4BNxkHXaj7xAjWPic';
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function saveMealPlan() {
+    supabaseClient
+      .from('meal_plans')
+      .insert({ meal_plan: mealData })
+      .then((response) => {
+        console.log('Meal plan saved:', response);
+      })
+      .catch((error) => {
+        console.log('Error saving meal plan:', error);
+      });
+  }
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=e49cbcb80d2b47f5a04fa076d9065574&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="title spotlight">
+        <div className="top">Welcome to the FoodLab</div>
+        <div className="bottom" aria-hidden="true">Welcome to the FoodLab</div>
+      </section>
+
+      <section className="controls">
+        <p>How many calories would you like to have in your meal?</p>
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        /> <br></br>
+        <button onClick={getMealData} id="button1" class="btn btn-primary">Get Meal Plan</button>
+
+        <button onClick={saveMealPlan} id="button2" class="btn btn-success">Save Meal Plan</button>
+
+
+      </section>
+      {mealData && <MealList mealData={mealData} />}
     </div>
   );
 }
---
+++ src/Meal.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "./index.css";
+
+export default function Meal({ meal }) {
+  const [imageUrl, setImageUrl] = useState("");
+  const [bgColor, setBgColor] = useState("white");
+
+  useEffect(() => {
+    fetch(
+      `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=e49cbcb80d2b47f5a04fa076d9065574&includeNutrition=false`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.image);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }, [meal.id]);
+
+  const colors = ["#D9D9D9", "#0396FF", "#32CCBC", "#3677FF", "#A64DB6", "#9452A5", "#58CFFB","#F067B4"];
+
+  useEffect(() => {
+    const bgInterval = setInterval(() => {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      setBgColor(randomColor);
+    }, 5000);
+
+    return () => clearInterval(bgInterval);
+  }, []);
+  
+  
+
+  return (
+    <article style={{ backgroundColor: bgColor }} className="bg-white rounded-md shadow-md overflow-hidden color-changing">
+      <h3 className="text-2xl font-bold mb-4 px-6 pt-6">{meal.title}</h3>
+      <div className="h-64 flex justify-center items-center">
+        <img src={imageUrl} alt="recipe" className="h-full object-cover rounded-md" />
+      </div>
+      <ul className="list-disc pl-8 my-6">
+        <li className="text-lg text-gray-600 mb-2">Preparation time: {meal.readyInMinutes} minutes</li>
+        <li className="text-lg text-gray-600 mb-4">Number of servings: {meal.servings}</li>
+      </ul>
+      <div className="px-6 pb-6 flex justify-center">
+        <a href={meal.sourceUrl} className="bg-black hover:bg-gray-700 text-white font-bold underline py-2 px-4 rounded">Go to Recipe</a>
+      </div>
+
+
+    </article>
+  );
+}
